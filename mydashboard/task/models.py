67f073e2660291c516b35076f0b0b8cf06ada4b2
Alejandro Bautista <abautista@psi.network>
2019-10-21 19:55:54 -0600

    The functionality for making the app to store the data works now.

--- mydashboard/task/models.py
+++ mydashboard/task/models.py
@@ -1,13 +1,11 @@
 # Python Libraries
-from datetime import date
-
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
-    responsible  = models.TextField(null=True)
+    responsible  = models.CharField(null=False, max_length=120, default='Alejandro Bautista')
     task = models.TextField(null=True)
-    initial_date = models.DateField(default=timezone.now())
+    initial_date = models.DateField(default=timezone.now(), null=False)
     ending_date  = models.DateField(null=True)
 
--- mydashboard/task/urls.py
+++ mydashboard/task/urls.py
@@ -5,7 +5,7 @@ from .views import (create_task,
                    update_task)
 
 urlpatterns = [
-    path('task-create/', create_task),
-    path('task-update/', update_task),
-    path('task-delete/', delete_task),
+    path('create/', create_task),
+    path('update/', update_task),
+    path('delete/', delete_task),
 ]
