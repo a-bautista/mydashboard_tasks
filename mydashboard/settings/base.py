1613ea4ddb954d3e62744de899b8d27fc1249bbf
Alejandro B <alex.bautista.ramos.90@gmail.com>
2019-12-09 12:09:21 -0700

    Deleted some lines from the database configuration.

--- mydashboard/settings/base.py
+++ mydashboard/settings/base.py
@@ -77,12 +77,7 @@ WSGI_APPLICATION = 'mydashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydashboard_tasks',
-        'USER': 'postgres',
-        'PASSWORD': 'Ab152211',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql'
     }
 }
 
