b512bfb70caf1b830badd87fedf992b1207cc9d3
Alejandro B <alex.bautista.ramos.90@gmail.com>
2019-12-12 21:31:45 -0700

    Updated the README with the instructions for creating the postgres db with a container and modified the settings.py to apply the css files.

--- mydashboard/settings.py
+++ mydashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zk6r!vjkf)1&4$m0m%dj(j5z(i6$jwq2)_(i0)2^04$sbyx1!)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['telos-dashboard-container.herokuapp.com', '0.0.0.0']
 
 
 # Application definition
@@ -78,7 +78,8 @@ WSGI_APPLICATION = 'mydashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
