8c1eb7482f2a1249b7cbd5300990ccb500af99ec
Alejandro B <alex.bautista.ramos.90@gmail.com>
2019-12-12 19:48:49 -0700

    Added the files for displaying the css files.

--- mydashboard/settings.py
+++ mydashboard/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,6 +82,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -119,3 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+#STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static', 'mydashboard'))
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
+
+# Static files for running your app in Live
+# Whitenoise is mandatory for making the gunicorn to work correctly
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+
+# Media files for running your app in Live
+MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static', 'media-root')
