e25cbf328ad64b510fe8b6ef8977a4509b965171
Alejandro B <alex.bautista.ramos.90@gmail.com>
2019-12-27 13:13:03 -0700

    Added the main graphs that will be displayed in the root page.

--- task/urls.py
+++ task/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 # import the component task
 from .views import (create_task, delete_task, update_task,
-                    view_previous_tasks, retrieve_all)
+                    view_previous_tasks, retrieve_all, home,
+                    Dashboard_Categories_Month, Dashboard_Status_Month,
+                    Dashboard_Tasks_Week)
+
+# Because I am redirecting the root address to the Task application, I need to include the task in the URL, so it gets defined in the lines below
 
 urlpatterns = [
-    path('tasks/', retrieve_all),
-    path('new/', create_task, name='create_task'),
-    path('update/<int:id>/', update_task, name='update_task'),
-    path('delete/<int:id>/', delete_task, name='delete_task'),
-    path('previous_tasks/', view_previous_tasks)
+    path('', home, name='home'), # main page
+    path('dashboard_categories_month', Dashboard_Categories_Month.as_view()), # load this view in the main page
+    path('dashboard_status_month', Dashboard_Status_Month.as_view()),         # load this view in the main page
+    path('dashboard_points_week', Dashboard_Tasks_Week.as_view()),
+    path('task/tasks/', retrieve_all),
+    path('task/new/', create_task, name='create_task'),
+    path('task/update/<int:id>/', update_task, name='update_task'),
+    path('task/delete/<int:id>/', delete_task, name='delete_task'),
+    path('task/previous_tasks/', view_previous_tasks)
 ]
