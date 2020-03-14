9c23ca3fc5ec35d9e2053c7ab69daf7431c76741
a-bautista <alex.bautista.ramos.90@gmail.com>
2020-03-14 14:45:15 -0600

    Telos 4.0.0 (Beta): Added the urls to the goals and updated the navbar.

---
+++ goal/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+# import the component task
+from .views import (    
+                        create_goal, 
+                        #update_goal,
+                        #view_previous_goals_quarter, 
+                        #view_previous_goals_yearly, 
+                        #retrieve_all, 
+                        #main_dashboard
+                    )
+
+# Because I am redirecting the root address to the Task application, 
+# I need to include the goal in the URL, so it gets defined in the lines below
+
+urlpatterns = [
+    #path('', main_dashboard, name='main_dashboard'), # main dashboard page
+    #path('list/', retrieve_all),
+    path('new/', create_goal, name='create_goal'),
+    #path('update/<int:id>/', update_goal, name='update_goal'),
+    #path('previous_goals_quarterly/', view_previous_goals_quarterly),
+    #path('previous_goals_yearly/', view_previous_goals_yearly)
+]
