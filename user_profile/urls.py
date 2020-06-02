48139a018300900dacd836afebe386f5c06de373
a-bautista <alex.bautista.ramos.90@gmail.com>
2020-06-02 13:39:25 -0600

    Telos 4.10.4:The settings for the task points and task life are being displayed.

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 # import the component task
-from .views import view_user_settings
+from .views import view_user_settings, view_app_settings
 
 urlpatterns = [
-    path('user/', view_user_settings, name='user_settings')
+    path('user/', view_user_settings, name='user_settings'),
+    path('app_settings/', view_app_settings, name='app_settings')
 ]
 
 if settings.DEBUG:
--- user_profile/views.py
+++ user_profile/views.py
@@ -19,3 +19,17 @@ def view_user_settings(request):
         if p_form.is_valid():
             p_form.save()
     return redirect('/main/')
+
+@login_required
+def view_app_settings(request):
+
+    if request.method == 'GET':
+        template_name = 'user_profile/app_settings.html'
+        s_form = ProfileUpdateForm(request.POST or None, instance=request.user.profile)
+        return render(request, template_name, {'form': s_form})
+
+    elif request.method == 'POST':
+        s_form = ProfileUpdateForm(request.POST or None, request.FILES, instance=request.user.profile)     
+        if s_form.is_valid():
+            s_form.save()
+    return redirect('/main/')
