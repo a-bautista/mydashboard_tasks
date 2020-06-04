1d634dc0a858f55ccb51a6cc9c001c9c70b69f9a
a-bautista <alex.bautista.ramos.90@gmail.com>
2020-06-03 19:07:32 -0600

    Telos 4.11.1: The reset password functionality and the registration options are working properly without the sendgrid configuration.

--- mydashboard/settings.py
+++ mydashboard/settings.py
@@ -159,3 +159,13 @@ LOGIN_URL = 'login'
 # External email settings
 EMAIL = os.getenv("EMAIL_HOST_USER")
 API_KEY = os.getenv("SENDGRID_API_KEY")
+
+
+# Reset password
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
